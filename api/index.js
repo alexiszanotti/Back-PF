11656ac3133e786d3cee8c3d6c3bb0fdcda09a28
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2021-11-09 13:31:25 -0600

    reformateado el json para quitar espacios

--- api/index.js
+++ api/index.js
@@ -1,11 +1,27 @@
 const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const { conn, Product, Brand } = require("./src/db.js");
 const catalogProducts = require("./src/parseJson/parsejson.js");
 
 conn.sync({ force: true }).then(() => {
+  //createDB();
   server.listen(3001, () => {
-    console.log("%s listening at 3001");
+    
+    console.log(catalogProducts[0]["Product ID"]);
+    console.log("%s listening at 3001 bla bla");
   });
 });
 
-
+const createDB = async () => {
+  // catalogProducts.forEach(product => {
+  //   const { ProductID, ProductName, ListingPrice, SalePrice, Discount, Images, Description, Brand } = product;
+  //     product.create({
+  //       productID: ProductID,
+  //       productName: ProductName,
+  //       listingPrice: ListingPrice,
+  //       salePrice: SalePrice,
+  //       discount: Discount,
+  //       images: Images,
+  //       description: Description,
+  //   }).then(e=> e.addBrand(Brand));
+  //});
+};
