883d41b22647cf027498d40b25a5009a3984f3dd
zanotti86 <alexiszanotti@gmail.com>
2021-11-09 18:14:27 +0100

    Initial commits

---
+++ api/src/models/Product.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('product', {
+   productID: {
+     type: DataTypes.INTEGER,
+     primaryKey: true,
+     allowNull: false,
+   },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    listingPrice: {
+      type: DataTypes.DECIMAL(10, 2), // 10 precision, 2 decimales
+      allowNull: false,
+    },
+    salePrice: {
+      type: DataTypes.DECIMAL(10, 2), // 10 precision, 2 decimales
+      allowNull: false,
+    },
+    //crear descuento con limite entre 0 y 100
+    discount: {
+      type: DataTypes.DECIMAL(10, 2), // 10 precision, 2 decimales
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 100,
+      },
+    },
+    //images es un arreglo de strings
+    images: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+  });
+};
