fb7ccd824739e6ccb6e6f3097e5e2de4913d4a76
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2021-11-12 13:01:00 -0600

    corregido filter Changes to be committed: 	modified:   api/src/controllers/getAllUsers.js 	modified:   api/src/controllers/getproductsinfo.js 	modified:   api/src/routes/getAllUsersRoute.js 	modified:   api/src/routes/getbrand.js

--- api/src/controllers/getAllUsers.js
+++ api/src/controllers/getAllUsers.js
@@ -1,41 +1,16 @@
 const { User, conn } = require("../db");
 
-const usersM = [
-  (user1 = {
-    UUID: "0345frsde23",
-    userName: "Jorge",
-    type: "user",
-    password: "1234",
-  }),
 
-  (user2 = {
-    UUID: "0345frsde30",
-    userName: "Abraham",
-    type: "Admin",
-    password: "1234admin",
-  }),
-
-  (user3 = {
-    UUID: "0345frsde40",
-    userName: "Pablo",
-    type: "user",
-    password: "1234",
-  }),
-
-  (user4 = {
-    UUID: "0345frsde58",
-    userName: "Ezequiel",
-    type: "user",
-    password: "1234",
-  }),
-];
 
 const getAllUsers = async () => {
-  let allUser = await User.findAll({
-    attributes: ["id", "userName", "password", "isLoggedIn", "type"],
-  });
 
-  return allUser;
-};
+    let allUser = await User.findAll({
+
+        attributes: ['id', 'userName','name','lastName', 'birthDay', 'password', 'gender', 'type'],
+
+    })
+
+    return allUser
+}
 
 module.exports = getAllUsers;
