26c1fb242a55a5ed090bf158d052932c346e95dd
eionbk <gamma-1994@hotmail.com>
2021-11-15 10:09:35 -0600

    refactoreada ruta /getAllUsers

--- api/src/controllers/getAllUsers.js
+++ api/src/controllers/getAllUsers.js
@@ -1,16 +1,33 @@
-const { User, conn } = require("../db");
+const { User } = require("../db");
 
+const getAllUsers = async (req, res, next) => {
+    const {id} = req.body;
+    try {
+        
+        if (id){ 
+           const user = await User.findOne({where: {id}})
+           console.log(user);
+            user ? res.status(200).json(user): res.status(404).send({error: "No hay usuario con ese nombre"});
+        }else  {
+            
+            let allUsers = await User.findAll({
+        
+             attributes: ['id', 'userName','name','lastName', 'birthDay', 'password', 'gender', 'type'],
+        
+            })
+            if (allUsers) return res.status(200).send(allUsers);
+            
+            else return res.status(404).send({ error: "No hay usuarios" });
+        }
+        
 
+    
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 
-const getAllUsers = async () => {
-
-    let allUser = await User.findAll({
-
-        attributes: ['id', 'userName','name','lastName', 'birthDay', 'password', 'gender', 'type'],
-
-    })
-
-    return allUser
+    
 }
 
 module.exports = getAllUsers;
--- api/src/routes/getAllUsersRoute.js
+++ api/src/routes/getAllUsersRoute.js
@@ -3,33 +3,7 @@ const getAllUsers = require("../controllers/getAllUsers");
 
 const router = Router();
 
-router.get("/getAllUsers", async (req, res, next) => {
-  try {
-    const allUser = await getAllUsers();
+router.get("/getAllUsers", getAllUsers);
 
-    if (allUser.length > 0) return res.status(200).send(allUser);
-    else return res.status(404).send({ error: "no hay usuarios" });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/getAllUsers/:name", async (req, res, next) => {
-  const { name, gender } = req.params;
-
-  try {
-    const allUser = await getAllUsers();
-
-    gender && (allUser = allUser.filter(e => e.name.charAt(0) === gender.charAt(0)));
-
-    const user = allUser.filter(e => e.userName === name);
-
-    if (user.length > 0) return res.status(200).send(user);
-
-    return res.status(404).send({ error: "el usuario no existe" });
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 module.exports = router;
