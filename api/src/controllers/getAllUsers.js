e8cd102565ae320452e62aa92096e7cdd3b33222
zanotti86 <alexiszanotti@gmail.com>
2021-11-17 10:33:18 +0100

    Back refactorizado

--- api/src/controllers/getAllUsers.js
+++ api/src/controllers/getAllUsers.js
@@ -3,21 +3,21 @@ const { User } = require("../db");
 async function getAllUsers(req, res, next) {
   const { id } = req.body;
   try {
-    if (id && id !== "") {
+    if (id) {
       const user = await User.findOne({
         where: {
-          id: id,
+          id,
         },
       });
       res.status(200).send(user);
     } else {
       const users = await User.findAll({
-        attributes: ["name", "lastName", "birthDay", "email", "userName", "type"],
+        attributes: ["id", "name", "lastName", "userName", "type", "birthDay", "gender"],
+      }).then(users => {
+        res.status(200).send(users);
       });
-      res.status(200).send(users);
     }
   } catch (error) {
-    console.log(error);
     next(error);
   }
 }
