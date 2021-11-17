2943e35d1f3ec2aafc0f0f4ab774d2753d1b3784
zanotti86 <alexiszanotti@gmail.com>
2021-11-17 19:24:29 +0100

    Modify Method Routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,6 @@ const usersRoute = require("./usersRoute");
 const productsRoute = require("./productsRoute");
 const categoriesRoute = require("./categoriesRoute");
 const reviewsRoute = require("./reviewsRoute");
-const filterRoute = require("./filterRoute");
 
 const router = Router();
 router.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -14,6 +13,5 @@ router.use("/products", productsRoute);
 router.use("/users", usersRoute);
 router.use("/categories", categoriesRoute);
 router.use("/reviews", reviewsRoute);
-router.use("/filter", filterRoute);
 
 module.exports = router;
