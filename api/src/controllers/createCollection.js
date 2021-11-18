3c014fc83ddcafaa7cddc9670d2afc31f90c2d3d
zanotti86 <alexiszanotti@gmail.com>
2021-11-18 12:58:04 +0100

    Create EndPoint CreateCollection

---
+++ api/src/controllers/createCollection.js
@@ -0,0 +1,22 @@
+const { Collection } = require("../db");
+
+async function createCollection(req, res, next) {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).send({
+        message: "El Nombre es requerido",
+      });
+    }
+    const collection = await Collection.create({
+      name,
+    });
+    return res.status(201).send({
+      collection,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { createCollection };
