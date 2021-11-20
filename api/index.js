5aa39214b8993c02973631954651f7d8a02e5203
Alexis Zanotti <alexiszanotti@gmail.com>
2021-11-20 19:10:49 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ const server = require("./src/app.js");
 const { conn, Product, Collection, User, Size } = require("./src/db.js");
 const adidasInfo = require("./src/parseJson/parsejson.js");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   //createDB();
   server.listen(3001, async () => {
     console.log("Espere a que se cree la base de datos..");
     console.time("Se creo la base de datos con exito");
     try {
-      await createDB();
+      //await createDB();
       console.timeEnd("Se creo la base de datos con exito");
       console.log("%s listening at 3001");
     } catch (error) {
