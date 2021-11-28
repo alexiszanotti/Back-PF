5170e090b5ca927cc52eff3a534e8616579629fa
Emanuel Germano <emanuelgermano90@gmail.com>
2021-11-28 12:13:34 -0300

    papa

--- api/src/controllers/mail/sendMail.js
+++ api/src/controllers/mail/sendMail.js
@@ -16,7 +16,7 @@ async function sendToMail(req, res, next) {
     let img = [
       
       "https://document-export.canva.com/hrFLs/DAEw8ohrFLs/77/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20211127%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20211127T042728Z&X-Amz-Expires=64522&X-Amz-Signature=a2892f75d48279badb0a77df29da358b8f6c19a6d6513180e4da6d6e17a96be8&X-Amz-SignedHeaders=host&response-expires=Sat%2C%2027%20Nov%202021%2022%3A22%3A50%20GMT", 
-      "https://document-export.canva.com/ky5Wk/DAEw8hky5Wk/6/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20211127%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20211127T112840Z&X-Amz-Expires=38888&X-Amz-Signature=5861a4828a6857e6a282d86221e07becd5e34530cc36ffcf1ef5039ffd598099&X-Amz-SignedHeaders=host&response-expires=Sat%2C%2027%20Nov%202021%2022%3A16%3A48%20GMT"
+      "https://document-export.canva.com/ky5Wk/DAEw8hky5Wk/9/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20211128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20211128T131505Z&X-Amz-Expires=13119&X-Amz-Signature=7286b54691379cbc472a2f2a12e65415f156fa7f59cc1910410cc70aa7dabfe7&X-Amz-SignedHeaders=host&response-expires=Sun%2C%2028%20Nov%202021%2016%3A53%3A44%20GMT"
     
     ]
 
