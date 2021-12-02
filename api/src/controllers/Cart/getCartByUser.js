1d1d9f71c131a2dbca50a244a3e6349363b82da1
enzo388 <83272052+enzo388@users.noreply.github.com>
2021-12-02 02:58:23 -0300

    reparacion errores 500 consola

--- api/src/controllers/Cart/getCartByUser.js
+++ api/src/controllers/Cart/getCartByUser.js
@@ -1,34 +1,43 @@
 const { Cart, ProductsInCart, Product } = require("../../db");
+var validator = require('validator');
 
 async function getCartByUser(req, res, next) {
   const { cartId } = req.query;
   try {
-    const cart = await Cart.findOne({
-      where: {
-        id: cartId,
-      },
-      attributes: ["id", "status"],
-      include: [
-        {
-          model: ProductsInCart,
-          attributes: {
-            exclude: ["id", "price", "quantity", "productName", "CartId"],
-          },
-          include: [
-            {
-              model: Product,
-              attributes: ["id", "productName", "salePrice", "images", "stock"],
-            },
-          ],
-        },
-      ],
-    });
-    if (!cart) {
+    if(!validator.isUUID(cartId)){
       return res.status(404).send({
-        message: "No se encontraron usuarios con ese ID.",
+        message: "el cartId no es de tipo uuid o es null ",
       });
+    }else{
+      const cart = await Cart.findOne({
+        where: {
+          id: cartId,
+        },
+        attributes: ["id", "status"],
+        include: [
+          {
+            model: ProductsInCart,
+            attributes: {
+              exclude: ["id", "price", "quantity", "productName", "CartId"],
+            },
+            include: [
+              {
+                model: Product,
+                attributes: ["id", "productName", "salePrice", "images", "stock"],
+              },
+            ],
+          },
+        ],
+      });
+      if (!cart) {
+        return res.status(404).send({
+          message: "No se encontraron usuarios con ese ID.",
+        });
+      }
+      return res.status(200).send(cart);
+
     }
-    return res.status(200).send(cart);
+    
   } catch (error) {
     next(error);
   }
--- api/src/controllers/Users/createUser.js
+++ api/src/controllers/Users/createUser.js
@@ -3,12 +3,18 @@ const { User, Cart } = require("../../db");
 async function createUser(req, res, next) {
   try {
     const { email, name, lastName, birthDay, gender } = req.body;
+    const auxuser = await User.findAll({ where: { email :  email} });
+    if(auxuser.length > 0 ){
+      res.status(400).send({ Msge: "este usuario ya esta creado" });
+    }else {
+      let cart = await Cart.create({});
+      let user = await User.create({ email, name, lastName, birthDay, gender, type: "User" });
+  
+      let completeUser = await user.setCart(cart);
+      return res.status(200).send({ Msge: "Usuario creado con exito", completeUser });
+    }
 
-    let cart = await Cart.create({});
-    let user = await User.create({ email, name, lastName, birthDay, gender, type: "User" });
-
-    let completeUser = await user.setCart(cart);
-    res.status(200).send({ Msge: "Usuario creado con exito", completeUser });
+   
   } catch (error) {
     next(error);
   }
