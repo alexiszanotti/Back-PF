cca5d74b3706853ef3d0ca9f4c09c4cc7991be7b
enzo388 <83272052+enzo388@users.noreply.github.com>
2021-12-07 16:11:42 -0300

    repair/createuser

--- api/src/controllers/Users/createUser.js
+++ api/src/controllers/Users/createUser.js
@@ -3,12 +3,27 @@ const { User, Cart } = require("../../db");
 async function createUser(req, res, next) {
   try {
     const { email, name, lastName, birthDay, gender } = req.body;
+    if(email && name &&  lastName &&  birthDay && gender){
+      const user = await User.findAll({
+        where: { email: email },
+      })
+      console.log(user)
+      if(user.length === 0){
+        console.log("entro aqui (12)")
+        let user1 = await User.create({ email, name, lastName, birthDay, gender, type: "User" });
+        let cart = await Cart.create({});
+        let completeUser = await user1.setCart(cart);
+        return res.status(200).send({ Msge: "Usuario creado con exito", completeUser });
+      }else {
+        console.log("entro aqui (18)")
+        return res.status(400).json({msg:"este usuario ya se encuentra registrado"})
+      }
 
-    let cart = await Cart.create({});
-    let user = await User.create({ email, name, lastName, birthDay, gender, type: "User" });
+    }else {
+      console.log("entro aqui (23)")
+      return res.status(400).json({msj : "falta algun campo"})
+    }
 
-    let completeUser = await user.setCart(cart);
-    res.status(200).send({ Msge: "Usuario creado con exito", completeUser });
   } catch (error) {
     next(error);
   }
